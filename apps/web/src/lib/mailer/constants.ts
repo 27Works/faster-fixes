8d10ee8ae9b31ad9ab75de94fb0ffa090f39160a
James Lambie <jameslambie@gmail.com>
2026-09-08 20:48:48 +1200

    Update email constants to use EMAIL_DOMAIN

--- apps/web/src/lib/mailer/constants.ts
+++ apps/web/src/lib/mailer/constants.ts
@@ -1,2 +1,2 @@
-export const NO_REPLY_EMAIL = `noreply@${process.env.DOMAIN_NAME}`;
-export const SENDER_EMAIL = `contact@${process.env.DOMAIN_NAME}`;
+export const NO_REPLY_EMAIL = `noreply@${process.env.EMAIL_DOMAIN}`;
+export const SENDER_EMAIL = `contact@${process.env.EMAIL_DOMAIN}`;
